ed2e4e40274f170d868621f60a964e60a80b2be5
Rodrigo Ochoa <roaloch@icloud.com>
2024-03-20 16:05:15 -0600

    ADD - REST API GET SUBMODELS ENDPOINT

---
+++ server/api/submodels.get.ts
@@ -0,0 +1,35 @@
+export default defineEventHandler(async (event) => {
+  const { year, make, model } = getQuery(event);
+
+  if (!year || !make || !model) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: '[E400-MSQ] Missing query parameter YEAR | MAKE | MODEL',
+    });
+  }
+
+  const { result }: { result: SubmodelsResponse[] } = await $fetch(
+    fitmentAtlasAPI.endpoints.submodels,
+    {
+      baseURL: fitmentAtlasAPI.baseURL,
+      query: {
+        makeID: make,
+        modelID: model,
+        productType: fitmentAtlasAPI.productTypes.wheel,
+        yearID: year,
+      },
+      headers: {
+        'FG-APIKEY': fitmentAtlasAPI.apiKey!,
+      },
+    }
+  );
+
+  const submodels: Submodel[] = result.map((e: SubmodelsResponse) => {
+    return <Submodel>{
+      key: e.vcdB_SubmodelID,
+      value: e.submodelName,
+    };
+  });
+
+  return submodels;
+});
--- utils/types.ts
+++ utils/types.ts
@@ -3,6 +3,12 @@ interface KeyValueResponse {
   value: string;
 }
 
+interface SubmodelsResponse {
+  submodelName: string;
+  vcdB_SubmodelID: number;
+}
+
 interface Year extends KeyValueResponse {}
 interface Make extends KeyValueResponse {}
 interface Model extends KeyValueResponse {}
+interface Submodel extends KeyValueResponse {}
